972c4a7ff1d7dfb3b7e551745e41f7c96a8e056d
Joshua-dos-santos <74915903+Joshua-dos-santos@users.noreply.github.com>
2024-03-18 16:50:18 +0100

    Django userName and password authentication

---
+++ Django/django_usernameandpassword/django_usernameandpasswordapp/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class DjangoUsernameandpasswordappConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'django_usernameandpasswordapp'
