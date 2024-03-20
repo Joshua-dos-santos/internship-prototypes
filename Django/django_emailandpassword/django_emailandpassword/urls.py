ef2109734e1af173fdba32903fd87dadc918fe19
Joshua-dos-santos <74915903+Joshua-dos-santos@users.noreply.github.com>
2024-03-20 14:12:02 +0100

    Django email and password - Updated so profile can be viewed

--- Django/django_emailandpassword/django_emailandpassword/urls.py
+++ Django/django_emailandpassword/django_emailandpassword/urls.py
@@ -1,11 +1,11 @@
 from django.contrib import admin
 from django.urls import path
 
-from django_emailandpasswordapp.views import LoginView, RegisterView
-
+from django_emailandpasswordapp.views import LoginView, RegisterView, ProfileView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('login/', LoginView.as_view(), name='login'),
-    path('register/', RegisterView.as_view(), name='register')
+    path('register/', RegisterView.as_view(), name='register'),
+    path('accounts/profile/', ProfileView.as_view(), name='profile'),
 ]
--- Django/django_emailandpassword/django_emailandpasswordapp/views.py
+++ Django/django_emailandpassword/django_emailandpasswordapp/views.py
@@ -1,16 +1,25 @@
 from django.contrib.auth import views as auth_views
 from django.views import generic
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import DetailView
 from django.urls import reverse_lazy
+from django.contrib.auth.models import User
 
 from .forms import LoginForm, RegisterForm
-
 
 class LoginView(auth_views.LoginView):
     form_class = LoginForm
     template_name = 'login.html'
-
+    success_url = reverse_lazy('profile')
 
 class RegisterView(generic.CreateView):
     form_class = RegisterForm
     template_name = 'register.html'
     success_url = reverse_lazy('login')
+
+class ProfileView(LoginRequiredMixin, DetailView):
+    model = User
+    template_name = 'profile.html'
+
+    def get_object(self, queryset=None):
+        return self.request.user
